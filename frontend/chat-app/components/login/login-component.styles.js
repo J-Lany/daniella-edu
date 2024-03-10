145d42e6c51170872d77a76883f3f864c6d2e801
J-Lany <ddzheylani@gmail.com>
2024-03-10 15:06:45 +0300

    Add adaptive

--- frontend/chat-app/components/login/login-component.styles.js
+++ frontend/chat-app/components/login/login-component.styles.js
@@ -11,18 +11,19 @@ const getLoginComponentStyle = () => {
       gap: 1rem;
     }
     .login-form__input {
-      font-size: 1rem;
-      padding: 1rem;
+      font-size: 1.5rem;
+      padding: 1.5rem;
       border: none;
       background: var(--light-gray-background);
       border-radius: 0.5rem;
     }
     .login-form__btn {
-      padding: 0.5rem 3.5rem;
+      padding: 1rem;
+      width: 30%;
+      font-size: 1.5rem;
       outline: none;
       border: 1px solid var(--light-gray-background);
       border-radius: 0.3rem;
-      font-size: 1rem;
       color: var(--gray-text-color);
       background-color: var(--light-gray-background);
       box-shadow: 0 1px 3px var(--gray-text-color);
@@ -32,10 +33,28 @@ const getLoginComponentStyle = () => {
     }
     .login-form__btn-group {
       display: flex;
-      justify-content: space-between;
+      justify-content: center;
+      gap: 2rem;
     }
     .login-form__btn:hover {
       background-color: var(--light-blue-background)
+    }
+    @media screen and (max-width: 600px) {
+      .login-form__btn {
+          padding: 0.75rem 3.5rem;
+          font-size: 1rem;
+          width: auto;
+        }
+      .login-form__btn-group {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        gap: 1rem;
+      }
+      .login-form__input {
+        font-size: 1rem;
+        padding: 1rem;
+      }
     }
   </style>
   `;
