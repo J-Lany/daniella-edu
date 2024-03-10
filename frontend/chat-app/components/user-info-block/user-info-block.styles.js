bb24c6620bef1c744b485a2424bb01dfb4241e6d
J-Lany <ddzheylani@gmail.com>
2024-03-10 15:16:12 +0300

    Add adaptive for header

--- frontend/chat-app/components/user-info-block/user-info-block.styles.js
+++ frontend/chat-app/components/user-info-block/user-info-block.styles.js
@@ -11,16 +11,28 @@ const getUserInfoBlockComponentStyle = () => {
         align-items: center;
       }
       .user-info__img {
-        max-width: 2.4rem;
-        max-height: 2.4rem;
+        max-width: 4.5rem;
+        max-height: 4.5rem;
         border-radius: 3.2rem;
       }
       .user-info__name {
-        font-size: 1.5rem;
+        font-size: 3rem;
       }
       .user-info__status {
-        font-size: 0.75rem;
+        font-size: 1.25rem;
         color:var(--gray-text-color);
+      }
+      @media screen and (max-width: 600px) {
+        .user-info__img {
+          max-width: 2.4rem;
+          max-height: 2.4rem;
+        }
+        .user-info__name {
+          font-size: 1.5rem;
+        }
+        .user-info__status {
+          font-size: 0.75rem;
+        }
       }
    </style>
   `;
