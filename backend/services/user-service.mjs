d712684dde36dc55e2a376b82219cc9ee7a72dce
J-Lany <ddzheylani@gmail.com>
2024-03-14 22:47:50 +0300

    Add hash pass

--- backend/services/user-service.mjs
+++ backend/services/user-service.mjs
@@ -1,11 +1,26 @@
+import bcrypt from "bcrypt";
+
 export class UserService {
   #users = new Map();
+
+  #hashPassword(password) {
+    const saltRounds = 7;
+    return bcrypt
+      .hash(password, saltRounds)
+      .then((hash) => hash)
+      .catch((err) => {
+        console.log("Ошибка хеширования пароля:", err);
+      });
+  }
   getUsers() {
     return this.#users;
   }
-  setUser({ login, email, password }) {
-    this.#users.set(login, { login, email, password });
+  async setUser({ login, email, password }) {
+    let hashedPassword = await this.#hashPassword(password);
+    this.#users.set(login, { login, email, hashedPassword });
+    console.log(this.#users);
   }
+
   isUserAlreadyExist(login) {
     return this.#users.has(login);
   }
