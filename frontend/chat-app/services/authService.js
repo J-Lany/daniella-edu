af0aa23eda53a4e5a435c232ad0579adaa9e95bc
J-Lany <ddzheylani@gmail.com>
2024-03-24 17:45:13 +0300

    Add post method in httpService

--- frontend/chat-app/services/authService.js
+++ frontend/chat-app/services/authService.js
@@ -1,14 +1,20 @@
+import { diContainer } from "../di/di";
+import { SERVICES } from "../di/api";
+
 const userSubscribers = new Set();
 const errorSubscribers = new Set();
 
+const mocUser = {
+  name: "Olga",
+  avatar:
+    "https://img.freepik.com/free-vector/hand-drawn-caricature-illustration_23-2149760515.jpg?t=st=1709056676~exp=1709060276~hmac=55f551634adbc78dfe62163921b8fd5ae89b2d05a7ef69f29f5cb3043d09ef1d&w=1380",
+  status: "Active",
+};
+
 export function authService() {
+  const httpServise = diContainer.resolve(SERVICES.http);
   let currentUser;
-  const mocUser = {
-    name: "Olga",
-    avatar:
-      "https://img.freepik.com/free-vector/hand-drawn-caricature-illustration_23-2149760515.jpg?t=st=1709056676~exp=1709060276~hmac=55f551634adbc78dfe62163921b8fd5ae89b2d05a7ef69f29f5cb3043d09ef1d&w=1380",
-    status: "Active",
-  };
+  
 
   function notifySubscribers() {
     userSubscribers.forEach((subscription) => {
