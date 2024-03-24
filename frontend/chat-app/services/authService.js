b6ff23a472f738b5bb29e6a7586c597c47a0a88c
J-Lany <ddzheylani@gmail.com>
2024-03-24 18:50:56 +0300

    Add token in auth service

--- frontend/chat-app/services/authService.js
+++ frontend/chat-app/services/authService.js
@@ -1,53 +1,51 @@
 import { diContainer } from "../di/di";
 import { SERVICES } from "../di/api";
 
-
 export class AuthService {
-   #userSubscribers = new Set();
-   #errorSubscribers = new Set();
-   #httpServise = diContainer.resolve(SERVICES.http);
-   #currentUser;
+  #userSubscribers = new Set();
+  #errorSubscribers = new Set();
+  #httpServise = diContainer.resolve(SERVICES.http);
+  #currentUser;
+  #currentUserToken;
 
-   notifySubscribers() {
+  notifySubscribers() {
     this.#userSubscribers.forEach((subscription) => {
       subscription(this.#currentUser);
     });
   }
 
-   notifyError(error) {
+  notifyError(error) {
     this.#errorSubscribers.forEach((subscription) => {
       subscription();
     });
   }
 
-   unSubscribe(subs) {
+  unSubscribe(subs) {
     this.#userSubscribers.delete(subs);
   }
 
-   subscribeCurrentUser(subscription) {
+  subscribeCurrentUser(subscription) {
     this.#userSubscribers.add(subscription);
     return () => this.unSubscribe(subscription);
   }
 
-   subscribeOnLoginError(subs) {
+  subscribeOnLoginError(subs) {
     this.#errorSubscribers.add(subs);
     return () => this.unSubscribe(subs);
   }
 
-  async  login(login, password) {
+  async login(login, password) {
     await this.#httpServise
       .post(`login/`, { login, password })
       .then((res) => {
-        console.log(res);
         this.#currentUser = res.user;
+        this.#currentUserToken = res.token;
         this.notifySubscribers();
       })
       .catch(this.notifyError);
   }
 
-  async  registration(login, password) {
+  async registration(login, password) {
     return await this.#httpServise.post(`registration/`, { login, password });
   }
-
- 
 }
