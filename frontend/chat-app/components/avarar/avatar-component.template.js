eb52b1fa7af149a423ebcc81274232e088e06b5d
J-Lany <ddzheylani@gmail.com>
2024-04-01 11:57:18 +0300

    Add fisrt name and last name check

--- frontend/chat-app/components/avarar/avatar-component.template.js
+++ frontend/chat-app/components/avarar/avatar-component.template.js
@@ -1,7 +1,10 @@
 import { getAvatarStyles } from "./avatar-component.styles";
 
-export function createAvatarTemplate({ avatar, login }) {
-  const abbreviation = `${login[0]}${login[login.length - 1]}`;
+export function createAvatarTemplate({ avatar, login, firstName, lastName }) {
+  const abbreviation =
+    firstName && lastName
+      ? `${firstName[0]}${lastName[0]}`
+      : `${login[0]}${login[login.length - 1]}`;
   return `
   ${getAvatarStyles()}
   <div class="avatar">
