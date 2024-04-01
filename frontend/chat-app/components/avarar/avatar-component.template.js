e60a03df4cdeb1c12550352be1c9e6eb25a40e73
J-Lany <ddzheylani@gmail.com>
2024-04-01 11:53:24 +0300

    Delete console.log

--- frontend/chat-app/components/avarar/avatar-component.template.js
+++ frontend/chat-app/components/avarar/avatar-component.template.js
@@ -2,7 +2,6 @@ import { getAvatarStyles } from "./avatar-component.styles";
 
 export function createAvatarTemplate({ avatar, login }) {
   const abbreviation = `${login[0]}${login[login.length - 1]}`;
-  console.log(getAvatarStyles());
   return `
   ${getAvatarStyles()}
   <div class="avatar">
