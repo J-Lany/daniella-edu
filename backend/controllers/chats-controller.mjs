c558af339c7634bae7cf580513ab26915236b2c7
J-Lany <ddzheylani@gmail.com>
2024-04-08 21:07:56 +0300

    Add get method detalrs

--- backend/controllers/chats-controller.mjs
+++ backend/controllers/chats-controller.mjs
@@ -4,58 +4,39 @@ import { SERVICES } from "../di/api.mjs";
 export function createChatsController(app) {
   const chatService = diContainer.resolve(SERVICES.chat);
 
-/**
- * @swagger
- * /chats/create-chat:
- *   post:
- *     summary: Создание чата
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               authorId:
- *                 type: string
- *               participantsId:
- *                 type: array
- *                 items:
- *                   type: string
- *     responses:
- *       '200':
- *         description: Успешное создание чата
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 chatId:
- *                   type: string
- *                   description: ID созданного чата
- *       '401':
- *         description: Такой чат уже существует
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   description: Сообщение об ошибке
- *       '500':
- *         description: Ошибка при создании чата
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 errorMessage:
- *                   type: string
- *                   description: Сообщение об ошибке при создании чата
- */
-
-
-
+  /**
+   * @swagger
+   * /chats/create-chat:
+   *   post:
+   *     summary: Создание чата
+   *     requestBody:
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               authorId:
+   *                 type: string
+   *               participantId:
+   *                 type: array
+   *                 items:
+   *                   type: string
+   *     responses:
+   *       '200':
+   *         description: Успешное создание чата
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 chatId:
+   *                   type: string
+   *                   description: ID созданного чата
+   *       '401':
+   *         description: Такой чат уже существует
+   *       '500':
+   *         description: Ошибка при создании чата
+   */
   app.post("/chats/create-chat", (req, res) => {
     const { authorId, participantsId } = req.body;
     try {
@@ -181,7 +162,20 @@ export function createChatsController(app) {
   app.get("/chats/{authorId}/", (req, res) => {
     const { authorId } = req.params;
     try {
-      chatService.getChatsByAythor(authorId);
-    } catch {}
+      const { chatId, date, participantsId } =
+        chatService.getChatsByAythor(authorId);
+      return res.status(200).json({ chatId, date, participantsId });
+    } catch (err) {
+      switch (err.message) {
+        case "401":
+          return res
+            .status(401)
+            .json({ message: "У данного пользователя нет чатов" });
+        default:
+          return res
+            .status(500)
+            .json({ message: "Ошибка в получении чатов, попробуйте позднее" });
+      }
+    }
   });
 }
