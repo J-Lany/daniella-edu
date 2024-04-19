2283134e6931d802754f8058402dd94753a4d80a
J-Lany <ddzheylani@gmail.com>
2024-04-19 11:36:51 +0200

    Add const

--- backend/utils/authorization.mjs
+++ backend/utils/authorization.mjs
@@ -1,11 +1,12 @@
 import { diContainer } from "../di/di.mjs";
 import { SERVICES } from "../di/api.mjs";
 
+const TOKEN_PREFIX = "Bearer ";
 export async function authorization(req, res, next) {
   const authService = diContainer.resolve(SERVICES.auth);
   const authorizationHeader = req.headers.authorization;
 
-  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+  if (!authorizationHeader || !authorizationHeader.startsWith(TOKEN_PREFIX)) {
     res.sendStatus(401);
   }
   const token = authorizationHeader.substring(7);
