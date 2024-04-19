8abeaeca3931f63e2eb7001ec0669662d85aa7a7
J-Lany <ddzheylani@gmail.com>
2024-04-19 10:48:55 +0200

    Add Bearer format

--- backend/utils/authorization.mjs
+++ backend/utils/authorization.mjs
@@ -1,16 +1,17 @@
 import { diContainer } from "../di/di.mjs";
 import { SERVICES } from "../di/api.mjs";
 
-export function authorization(req, res, next) {
+export async function authorization(req, res, next) {
   const authService = diContainer.resolve(SERVICES.auth);
   const authorizationHeader = req.headers.authorization;
 
-  if (!authorizationHeader) {
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
     res.sendStatus(401);
   }
-  if (authService.isAuth(authorizationHeader)) {
+  const token = authorizationHeader.substring(7);
+  if (await authService.isAuth(token)) {
     next();
   } else {
-    return res.sendStatus(403); 
+    return res.sendStatus(403);
   }
 }
