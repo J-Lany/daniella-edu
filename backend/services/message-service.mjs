226c89a2d2026a30546bbd82f567d500997ab2a8
J-Lany <ddzheylani@gmail.com>
2024-04-28 12:58:35 +0300

    Fix by comments

--- backend/services/message-service.mjs
+++ backend/services/message-service.mjs
@@ -9,47 +9,60 @@ export class MessageService {
 
   async getMessagesByChat(chatId, messagesPerPage, pageNumber) {
     const messagesList = await this.#messagesDao.getMessagesByChat(chatId);
+
     return paginator(messagesPerPage, pageNumber, messagesList);
   }
 
   async addMessage(authorId, chatId, messageBody) {
     const isPatrtisipant = await this.isPatrtisipant(authorId);
-    if (!isPatrtisipant) throw new Error(403);
+
+    if (!isPatrtisipant) {
+      throw new Error(403);
+    }
 
     const messageId = uuidv4();
     const createDate = new Date();
-
     const result = await this.#messagesDao.addMessage(chatId, {
       authorId,
       messageId,
       createDate,
       messageBody,
     });
+
     if (!result) {
       throw new Error(500);
     }
+
     return result;
   }
 
   async updateMessage(chatId, messageId, messageBody) {
     const isPatrtisipant = await this.isPatrtisipant(authorId);
-    if (!isPatrtisipant) throw new Error(403);
+
+    if (!isPatrtisipant) {
+      throw new Error(403);
+    }
 
     const result = await this.#messagesDao.updateMessage(
       chatId,
       messageId,
       messageBody
     );
+
     if (!result) {
       throw new Error(500);
     }
+
     return;
   }
 
   async deleteMessage(chatId, messageId) {
     try {
       const isPatrtisipant = await this.isPatrtisipant(authorId);
-      if (!isPatrtisipant) throw new Error(403);
+
+      if (!isPatrtisipant) {
+        throw new Error(403);
+      }
 
       await this.#messagesDao.deleteMessage(chatId, messageId);
     } catch {
@@ -59,13 +72,11 @@ export class MessageService {
 
   async isPatrtisipant(authorId) {
     const chats = await this.#chatsDao.getChatsByAuthor(authorId);
-    if (!chats) return false;
 
-    const isPatrtisipant = chats.find((chat) =>
-      chat.participantsIds.includes(authorId)
-    );
-    if (!isPatrtisipant) return false;
+    if (!chats) {
+      return false;
+    }
 
-    return true;
+    return chats.some((chat) => chat.participantsIds.includes(authorId));
   }
 }
