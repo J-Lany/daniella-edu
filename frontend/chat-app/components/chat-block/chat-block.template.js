45f265f55a23468cfe986a964289bae92dd59b4b
J-Lany <ddzheylani@gmail.com>
2024-04-30 22:58:02 +0300

    Add localhost

--- frontend/chat-app/components/chat-block/chat-block.template.js
+++ frontend/chat-app/components/chat-block/chat-block.template.js
@@ -3,8 +3,9 @@ import { getChatBlockStyle } from "./chat-block.styles";
 export function createChatBlockTemplate(messages) {
   if (!messages) {
     return `
-    <div class="messages">
-      Упс... тут ничего нет. Нажмите на + и выберите с кем хотите переписываться
+    ${getChatBlockStyle()}
+    <div class="chat-empty">
+      Упс... тут ничего нет. Нажмите на <span class="chat-plus">+</span> и выберите с кем хотите переписываться
     </div>`;
   }
   return `
--- frontend/chat-app/services/httpService.js
+++ frontend/chat-app/services/httpService.js
@@ -1,4 +1,4 @@
-export function httpService(baseUrl = "http://192.168.1.9:3000") {
+export function httpService(baseUrl = "http://localhost300:3000") {
   async function get(url, headers) {
     const response = await fetch(`${baseUrl}/${url}`, { headers });
 
