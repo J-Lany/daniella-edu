cf713b5e7f314073edd75bfc994fb3d9945dece7
J-Lany <ddzheylani@gmail.com>
2024-05-13 22:10:39 +0300

    Fix

--- frontend/chat-app/components/sidebar-block/sidebar-block.template.js
+++ frontend/chat-app/components/sidebar-block/sidebar-block.template.js
@@ -1,16 +1,18 @@
 import { getSidebarBlockStyle } from "./sidebar-block.styles";
 
-export function createSidebarBlockTemplate(list, type) {
-  const layoutList = () => {
-    return list.map((item) => {
+const layoutList = (list) => {
+  return list
+    .map((item) => {
       return `<message-component class="sidebar-block__item" user-id=${item.userId} display-mode="sidebar"></message-component> `;
-    });
-  };
+    })
+    .join("");
+};
 
+export function createSidebarBlockTemplate(list) {
   return `
     ${getSidebarBlockStyle()}
     <div class="sidebar-block">
-      ${list && list.length > 0 ? layoutList().join("") : "Упс, тут ничего нет"}
+      ${list && list.length > 0 ? layoutList(list) : "Упс, тут ничего нет"}
     </div>
 `;
 }
