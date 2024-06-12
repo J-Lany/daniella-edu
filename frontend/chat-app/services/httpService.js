611b60604c7666bd8a5c530b98a32515edaa6d28
J-Lany <ddzheylani@gmail.com>
2024-06-12 17:57:08 +0300

    Add refresh on front

--- frontend/chat-app/services/httpService.js
+++ frontend/chat-app/services/httpService.js
@@ -1,6 +1,5 @@
 import packageJson from "../../package.json";
-import { ACCESS_TOKEN } from "./authService";
-import { REFRESH_TOKEN } from "./authService";
+import { ACCESS_TOKEN, USER, REFRESH_TOKEN } from "./authService";
 
 export function httpService(baseUrl = packageJson.baseUrl) {
   async function get(url) {
@@ -8,7 +7,15 @@ export function httpService(baseUrl = packageJson.baseUrl) {
     const headers = createAutoruzarionHeader();
     const response = await fetch(fullUrl, { headers });
 
-    return await response.json();
+    if (response.status !== 405) {
+      return await response.json();
+    }
+
+    const isTokenRefreshed = await refreshToken();
+    if (isTokenRefreshed) {
+      const retryResponse = await fetch(fullUrl, { headers });
+      return await retryResponse.json();
+    }
   }
 
   async function post(url, payload) {
@@ -34,4 +41,33 @@ export function httpService(baseUrl = packageJson.baseUrl) {
     get,
     post,
   };
+}
+
+async function refreshToken(baseUrl = packageJson.baseUrl) {
+  const token = sessionStorage.getItem(REFRESH_TOKEN);
+  const user = sessionStorage.getItem(USER);
+
+  const requestBody = {
+    userId: user.userId,
+    refreshToken: token,
+  };
+
+  const response = await fetch(`${baseUrl}/refresh-token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (response.status === 200) {
+    const content = await response.json();
+
+    sessionStorage.setItem(REFRESH_TOKEN, content.refreshToken);
+    sessionStorage.setItem(ACCESS_TOKEN, content.accessToken);
+
+    return true;
+  }
+
+  return false;
 }
