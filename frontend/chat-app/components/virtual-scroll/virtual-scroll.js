6754731aa7f08063b6219cc48a4316e6fb5efd07
Daniella J-Lany <ddzheylani@gmail.com>
2024-07-26 18:23:35 +0300

    Add private method

--- frontend/chat-app/components/virtual-scroll/virtual-scroll.js
+++ frontend/chat-app/components/virtual-scroll/virtual-scroll.js
@@ -1,10 +1,20 @@
 import { createVSComponentTemplate } from "./virtual-scroll.template.js";
 
+const virtualScrollAttribute = {
+  CUSTOM_CSS: "custom-css"
+};
+
 export class VirtualScroll extends HTMLElement {
   itemsMap = [];
   visibleItems = new Set();
   itemHeights = [];
   bufferSize = 10;
+
+  #ATTRIBUTE_MAPPING = new Map([[virtualScrollAttribute.CUSTOM_CSS, this.#setCustomCSS.bind(this)]]);
+
+  static get observedAttributes() {
+    return Object.values(virtualScrollAttribute);
+  }
 
   static get name() {
     return "virtual-scroll";
@@ -15,23 +25,32 @@ export class VirtualScroll extends HTMLElement {
     this.attachShadow({ mode: "open" });
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      const callback = this.#ATTRIBUTE_MAPPING.get(name);
+      if (callback) {
+        callback(newValue);
+      }
+    }
+  }
+
+  #setCustomCSS(customCSS) {
+    let styleElement = this.shadowRoot.querySelector("style");
+    if (!styleElement) {
+      styleElement = document.createElement("style");
+      this.shadowRoot.appendChild(styleElement);
+    }
+
+    styleElement.textContent += customCSS;
+  }
+
   connectedCallback() {
-    this.render();
-    this.applyCustomStyles();
+    this.#render();
 
     this.indexItems();
     this.updateVisibleItems();
 
-    this.attachScrollListener();
-  }
-
-  applyCustomStyles() {
-    if (this.hasAttribute("custom-css")) {
-      const customCSS = this.getAttribute("custom-css");
-      const style = document.createElement("style");
-      style.textContent = customCSS;
-      this.shadowRoot.appendChild(style);
-    }
+    this.scrollTop = this.scrollHeight;
   }
 
   indexItems() {
@@ -39,27 +58,27 @@ export class VirtualScroll extends HTMLElement {
     this.itemsMap = children.map((child) => child.cloneNode(true));
 
     this.innerHTML = "";
-    this.itemHeights = this.itemsMap.map((item) => this.getItemHeight(item));
+    this.itemHeights = this.itemsMap.map((item) => this.#getItemHeight(item));
 
-    this.updatePlaceholderHeight();
+    this.#updatePlaceholderHeight();
   }
 
-  updatePlaceholderHeight() {
+  #updatePlaceholderHeight() {
     const totalHeight = this.itemHeights.reduce((sum, height) => sum + height, 0);
     this.shadowRoot.querySelector(".bottom-placeholder").style.height = `${totalHeight}px`;
   }
 
   updateVisibleItems() {
     const scrollTop = this.scrollTop;
-    const startIndex = this.getStartIndex(scrollTop);
-    const endIndex = this.getEndIndex(scrollTop);
+    const startIndex = this.#getStartIndex(scrollTop);
+    const endIndex = this.#getEndIndex(scrollTop);
 
-    this.removeInvisibleItems(startIndex, endIndex);
-    this.renderVisibleItems(startIndex, endIndex);
+    this.#removeInvisibleItems(startIndex, endIndex);
+    this.#renderVisibleItems(startIndex, endIndex);
   }
 
-  getStartIndex(scrollTop) {
-    const totalHeight = this.calculateTotalHeight(this.itemHeights, (acc) => acc.total >= scrollTop);
+  #getStartIndex(scrollTop) {
+    const totalHeight = calculateTotalHeight(this.itemHeights, (acc) => acc.total >= scrollTop);
 
     if (totalHeight.index === 0) {
       this.dispatchEvent(new CustomEvent("top-reached"));
@@ -68,11 +87,8 @@ export class VirtualScroll extends HTMLElement {
     return Math.max(totalHeight.index, 0);
   }
 
-  getEndIndex(scrollTop) {
-    const totalHeight = this.calculateTotalHeight(
-      this.itemHeights,
-      (acc) => acc.total >= scrollTop + this.clientHeight
-    );
+  #getEndIndex(scrollTop) {
+    const totalHeight = calculateTotalHeight(this.itemHeights, (acc) => acc.total >= scrollTop + this.clientHeight);
 
     if (totalHeight.index + this.bufferSize >= this.itemHeights.length) {
       this.dispatchEvent(new CustomEvent("bottom-reached"));
@@ -81,25 +97,13 @@ export class VirtualScroll extends HTMLElement {
     return Math.min(totalHeight.index + this.bufferSize, this.itemHeights.length - 1);
   }
 
-  calculateTotalHeight(itemHeights, predicate) {
-    return itemHeights.reduce(
-      (acc, height, index) => {
-        if (predicate(acc)) {
-          return acc;
-        }
-        return { total: acc.total + height, index };
-      },
-      { total: 0, index: -1 }
-    );
-  }
-
-  removeInvisibleItems(startIndex, endIndex) {
+  #removeInvisibleItems(startIndex, endIndex) {
     Array.from(this.visibleItems)
       .filter((i) => i < startIndex || i >= endIndex)
-      .forEach(this.removeItem, this);
+      .forEach(this.#removeItem, this);
   }
 
-  removeItem(i) {
+  #removeItem(i) {
     this.visibleItems.delete(i);
     const item = this.shadowRoot.querySelector(`[data-index="${i}"]`);
     if (item) {
@@ -107,7 +111,7 @@ export class VirtualScroll extends HTMLElement {
     }
   }
 
-  renderVisibleItems(startIndex, endIndex) {
+  #renderVisibleItems(startIndex, endIndex) {
     const content = this.shadowRoot.querySelector(".content");
 
     const fragment = Array.from({ length: endIndex - startIndex + 1 }, (_, index) => startIndex + index)
@@ -117,7 +121,7 @@ export class VirtualScroll extends HTMLElement {
         const item = this.itemsMap[i].cloneNode(true);
 
         item.style.position = "absolute";
-        item.style.top = `${this.getAccumulatedHeight(i)}px`;
+        item.style.top = `${this.#getAccumulatedHeight(i)}px`;
         item.style.width = "100%";
         item.setAttribute("data-index", i);
 
@@ -128,11 +132,11 @@ export class VirtualScroll extends HTMLElement {
     content.appendChild(fragment);
   }
 
-  getAccumulatedHeight(index) {
+  #getAccumulatedHeight(index) {
     return this.itemHeights.slice(0, index).reduce((sum, height) => sum + height, 0);
   }
 
-  getItemHeight(item) {
+  #getItemHeight(item) {
     const tempElement = item.cloneNode(true);
 
     tempElement.style.position = "absolute";
@@ -145,9 +149,24 @@ export class VirtualScroll extends HTMLElement {
     return height;
   }
 
-  attachScrollListener() {
-    this.scrollTop = this.scrollHeight;
+  #attachScrollListener() {
     this.addEventListener("scroll", this.updateVisibleItems.bind(this));
+  }
+
+  #detachScrollListener() {
+    this.removeEventListener("scroll", this.updateVisibleItems.bind(this));
+  }
+
+  scrollToTop() {
+    this.scrollTop = 0;
+    this.updateVisibleItems();
+    this.dispatchEvent(new CustomEvent("top-reached"));
+  }
+
+  scrollToBottom() {
+    this.scrollTop = this.scrollHeight;
+    this.updateVisibleItems();
+    this.dispatchEvent(new CustomEvent("bottom-reached"));
   }
 
   loadMoreItems(newItems) {
@@ -158,17 +177,33 @@ export class VirtualScroll extends HTMLElement {
     const indexednewItems = newItems.map((child) => child.cloneNode(true));
 
     this.itemsMap = [...indexednewItems, ...this.itemsMap];
-    this.itemHeights = this.itemsMap.map((item) => this.getItemHeight(item));
+    this.itemHeights = this.itemsMap.map((item) => this.#getItemHeight(item));
 
     this.updateVisibleItems();
 
     this.scrollTop = this.scrollHeight;
   }
 
-  render() {
+  #render() {
+    this.#detachScrollListener();
+
     const templateElm = document.createElement("template");
     templateElm.innerHTML = createVSComponentTemplate();
 
     this.shadowRoot.appendChild(templateElm.content.cloneNode(true));
+
+    this.#attachScrollListener();
   }
+}
+
+function calculateTotalHeight(itemHeights, predicate) {
+  return itemHeights.reduce(
+    (acc, height, index) => {
+      if (predicate(acc)) {
+        return acc;
+      }
+      return { total: acc.total + height, index };
+    },
+    { total: 0, index: -1 }
+  );
 }
