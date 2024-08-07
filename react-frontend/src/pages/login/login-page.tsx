e76d4a76874111a3b5fd8ac432702cc41c565e1a
Daniella J-Lany <ddzheylani@gmail.com>
2024-08-07 15:27:15 +0300

    Add fix

--- react-frontend/src/pages/login/login-page.tsx
+++ react-frontend/src/pages/login/login-page.tsx
@@ -1,27 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { clearError } from "../../redux/slices/authSlice";
+import { clearError } from "../../redux/slices/authSlice";
 import { loginAsync } from "../../redux/thunks/authThunks";
 import { AuthData } from "../../types/AuthData";
 import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./styles.module.css";
 import Button, { ButtonStyle } from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
+import Toast from "../../components/Toast/Toast";
+import { ToastType } from "../../components/Toast/Toast";
 
 function LoginPage() {
   const { register, handleSubmit } = useForm<AuthData>({ mode: "onChange" });
   const navigate = useNavigate();
 
-  // const error = useSelector((state: any) => state.auth.error);
+  const error = useSelector((state: any) => state.auth.error);
   const dispatch = useDispatch();
 
   const onSubmit: SubmitHandler<AuthData> = async (data: AuthData) => {
-   await dispatch(loginAsync(data) as any);
-   navigate("/");
+    await dispatch(loginAsync(data) as any);
   };
 
   const handleSignup = () => {
     navigate("/registration");
+  };
+
+  const closeErrorToast = () => {
+    dispatch(clearError());
   };
 
   return (
@@ -34,6 +39,7 @@ function LoginPage() {
           Don't have an account?{" "}
           <Button text="Sign up" type="button" style={ButtonStyle.Light} onClick={handleSignup} />
         </div>
+        {error && <Toast message={error} type={ToastType.Error} handleClose={closeErrorToast} />}
       </form>
     </div>
   );
