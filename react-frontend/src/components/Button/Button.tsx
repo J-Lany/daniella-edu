90830af84a4c4e30a51666770587512f0c1d4417
Daniella J-Lany <ddzheylani@gmail.com>
2024-08-15 16:16:11 +0300

    Add correct using classnames

--- react-frontend/src/components/Button/Button.tsx
+++ react-frontend/src/components/Button/Button.tsx
@@ -18,7 +18,11 @@ interface ButtonProps {
 const EMPTY_CALLBACK = () => {};
 
 const Button: React.FC<ButtonProps> = ({ text, className, type, onClick = EMPTY_CALLBACK }) => {
-  const buttonClass = classNames(styles.button, (className && styles[className]) || styles.default);
+  const buttonClass = classNames(styles.button, {
+    [styles.primary]: className === ButtonStyle.Primary,
+    [styles.default]: !className || className === ButtonStyle.Default,
+    [styles.light]: className === ButtonStyle.Light
+  });
 
   return (
     <button type={type} className={buttonClass} onClick={onClick}>
